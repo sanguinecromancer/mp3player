9da871a7b3716602b0fab44582fc0d91533dfc72
necromancer <sanguinecromancer@users.noreply.github.com>
2016-05-31 12:14:02 +0300

    Update ViewController.swift

--- MP3Player/ViewController.swift
+++ MP3Player/ViewController.swift
@@ -1,27 +1,3 @@
-//
-//  ViewController.swift
-//  MP3Player
-//
-//  Created by Zeynep Aykal on 04/04/16.
-//  Copyright © 2016 Zeynep Aykal. All rights reserved.
-//
-
-import UIKit
-
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
-}
 
 //
 //  ViewController.swift
